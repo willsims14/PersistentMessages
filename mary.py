6fcd9f7ed16c0627c384300dfdca4577284bcd42
William A Sims III <willsims14@gmail.com>
2017-04-14 14:34:19 -0500

    Removed unecesary parenthesis

--- mary.py
+++ mary.py
@@ -16,17 +16,17 @@ if __name__ == '__main__':
 	mary.otherMessages = mary.messagesDict['Margaret']
 
 	if len(sys.argv) == 2:
-		if(sys.argv[1] == 'ls'):
+		if sys.argv[1] == 'ls':
 			# Print the message log
 			mary.printMessageLog()
 		else:
 			try:
-				if(sys.argv[1] == 'delete'):
+				if sys.argv[1] == 'delete':
 					for index, msg in enumerate(mary.messagesDict['Mary']):
 						print(' -' + str(index) + ': ' + msg)
 					user_input = input(" > Which one?\n > ")
 					del mary.messagesDict['Mary'][int(user_input)]
-				elif(sys.argv[1] == 'add'):
+				elif sys.argv[1] == 'add':
 					# Get user's new message and extend dictionary with it
 					user_input = input(" > What do you want to say?\n > ")
 					mary.messagesDict['Mary'].extend([str(user_input)])
--- person.py
+++ person.py
@@ -3,48 +3,50 @@ import sys
 
 
 class Person:
-	def __init__(self, name):
-		'''
-		When a margaret is created, serialize and store the messages
-		into self.messagesDict
+    """ Class """
 
-		'''
-		self.name = name
-		self.messagesDict = dict()
-		self.myMessages = list()
-		self.otherMessages = list()
-		# Reads dictionary object
-		self.deserialize()
+    """
+    When a margaret is created, serialize and store the messages
+    into self.messagesDict """
+
+    def __init__(self, name):
+        """ """
+        self.name = name
+        self.messagesDict = dict()
+        self.myMessages = list()
+        self.otherMessages = list()
+        # Reads dictionary object
+        self.deserialize()
 
 
-	def printMyMessages(self):
-		print(self.myMessages)
-		return self.myMessages
+    def printMyMessages(self):
+        print(self.myMessages)
+        return self.myMessages
 
-	def serialize(self):
-		with open('messages.pkl', 'wb')	as writer:
-			pickle.dump(self.messagesDict, writer)
+    def serialize(self):
+        with open('messages.pkl', 'wb')	as writer:
+            pickle.dump(self.messagesDict, writer)
 
-	def deserialize(self):
-		with open('messages.pkl', 'rb') as reader:
-			data = pickle.load(reader)
-			self.messagesDict = data
-			return self.messagesDict
+    def deserialize(self):
+        with open('messages.pkl', 'rb') as reader:
+            data = pickle.load(reader)
+            self.messagesDict = data
+            return self.messagesDict
 
-	def printMessageLog(self):
-		# Get highest number of messages from a single user
-		max_length = len(self.myMessages)
-		if(len(self.otherMessages) > max_length):
-			max_length = len(self.otherMessages)
+    def printMessageLog(self):
+        # Get highest number of messages from a single user
+        max_length = len(self.myMessages)
+        if(len(self.otherMessages) > max_length):
+            max_length = len(self.otherMessages)
 
-		for i in range(0, max_length):
-			try:
-				print("({}) MARY     -->  {}".format(i, self.myMessages[i]))
-			except:
-				print("     MARY     -->  __NO RESPONSE__")
-				pass
-			try:
-				print("    MARGARET -->  {}".format(self.otherMessages[i]))
-			except:
-				print("    MARGARET -->  __NO RESPONSE__")
-				pass
+        for i in range(0, max_length):
+            try:
+                print("({}) MARY     -->  {}".format(i, self.myMessages[i]))
+            except:
+                print("     MARY     -->  __NO RESPONSE__")
+                pass
+            try:
+                print("    MARGARET -->  {}".format(self.otherMessages[i]))
+            except:
+                print("    MARGARET -->  __NO RESPONSE__")
+                pass
