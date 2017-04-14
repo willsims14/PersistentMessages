54c3da204f582e23eb74dc0a13833c6b73e300f8
William A Sims III <willsims14@gmail.com>
2017-04-14 14:32:58 -0500

    Removed parens

--- margaret.py
+++ margaret.py
@@ -16,17 +16,17 @@ if __name__ == '__main__':
 	margaret.otherMessages = margaret.messagesDict['Mary']
 
 	if len(sys.argv) == 2:
-		if(sys.argv[1] == 'ls'):
+		if sys.argv[1] == 'ls':
 			# Print the message log
 			margaret.printMessageLog()
 		else:
 			try:
-				if(sys.argv[1] == 'delete'):
+				if sys.argv[1] == 'delete':
 					for index, msg in enumerate(margaret.messagesDict['Margaret']):
 						print(' -' + str(index) + ': ' + msg)
 					user_input = input(" > Which one?\n > ")
 					del margaret.messagesDict['Margaret'][int(user_input)]
-				elif(sys.argv[1] == 'add'):
+				elif sys.argv[1] == 'add':
 					# Get user's new message and extend dictionary with it
 					user_input = input(" > What do you want to say?\n > ")
 					margaret.messagesDict['Margaret'].extend([str(user_input)])
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 					margaret.myMessages = margaret.messagesDict['Margaret']
 
 					print("--------  Message Log --------")
-					margaret.printMessageLog(margaret.messagesDict)
+					margaret.printMessageLog()
 				else:
 					print(' > "{}" command not found.'.format(sys.argv[1]))
 				# Serialize any changes that were made above to the persistant file (messages.pkl)
@@ -46,3 +46,4 @@ if __name__ == '__main__':
 		margaret.printMessageLog()
 	else:
 		print("Too many arguments!")
+		
