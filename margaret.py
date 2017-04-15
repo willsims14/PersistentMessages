d6e6457d2aeca8ee87f04d1120ac7d37acf0ad3d
William A Sims III <willsims14@gmail.com>
2017-04-15 12:38:15 -0500

    Halts execution if user doesn't give command line parameters

--- margaret.py
+++ margaret.py
@@ -11,39 +11,54 @@ from person import Person
 
 
 if __name__ == '__main__':
-	margaret = Person('Margaret')
-	margaret.myMessages = margaret.messagesDict['Margaret']
-	margaret.otherMessages = margaret.messagesDict['Mary']
+    margaret = Person('Margaret')
+    margaret.myMessages = margaret.messagesDict['Margaret']
+    margaret.otherMessages = margaret.messagesDict['Mary']
 
-	if len(sys.argv) == 2:
-		if sys.argv[1] == 'ls':
-			# Print the message log
-			margaret.printMessageLog()
-		else:
-			try:
-				if sys.argv[1] == 'delete':
-					for index, msg in enumerate(margaret.messagesDict['Margaret']):
-						print(' -' + str(index) + ': ' + msg)
-					user_input = input(" > Which one?\n > ")
-					del margaret.messagesDict['Margaret'][int(user_input)]
-				elif sys.argv[1] == 'add':
-					# Get user's new message and extend dictionary with it
-					user_input = input(" > What do you want to say?\n > ")
-					margaret.messagesDict['Margaret'].extend([str(user_input)])
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'ls':
+            # Print the message log
+            margaret.printMessageLog()
+        else:
+            try:
+                if sys.argv[1] == 'delete':
+                    for index, msg in enumerate(margaret.messagesDict['Margaret']):
+                        print(' -' + str(index) + ': ' + msg)
+                    user_input = input(" > Which one?\n > ")
+                    del margaret.messagesDict['Margaret'][int(user_input)]
+                elif sys.argv[1] == 'add':
+                    # Get user's new message and extend dictionary with it
+                    user_input = input(" > What do you want to say?\n > ")
+                    margaret.messagesDict['Margaret'].extend([str(user_input)])
 
-					margaret.myMessages = margaret.messagesDict['Margaret']
+                    margaret.myMessages = margaret.messagesDict['Margaret']
 
-					print("--------  Message Log --------")
-					margaret.printMessageLog()
-				else:
-					print(' > "{}" command not found.'.format(sys.argv[1]))
-				# Serialize any changes that were made above to the persistant file (messages.pkl)
-				margaret.serialize()
-			except:
-				pass
+                    print("--------  Message Log --------")
+                    margaret.printMessageLog()
+                else:
+                    print(' > "{}" command not found.'.format(sys.argv[1]))
+                # Serialize any changes that were made above to the persistant file (messages.pkl)
+                margaret.serialize()
+            except:
+                pass
 
-	elif len(sys.argv) == 1:
-		margaret.printMessageLog()
-	else:
-		print("Too many arguments!")
-		
+    elif len(sys.argv) == 1:
+        user_input = input("What do you want to do?\n > ")
+        
+        if user_input == 'quit':
+            print("Quitting")
+        elif user_input == 'add':
+            new_user_input = input("What do you want to say?\n > ")
+            margaret.messagesDict['Margaret'].extend([str(new_user_input)])
+            margaret.printMessageLog()
+        elif user_input == 'delete':
+            for index, msg in enumerate(margaret.messagesDict['Margaret']):
+                print(' -' + str(index) + ': ' + msg)
+            new_user_input = input(" > Which one?\n > ")
+            del margaret.messagesDict['Margaret'][int(new_user_input)]
+            margaret.printMessageLog()
+        elif user_input == 'ls':
+            margaret.printMessageLog()
+    else:
+        print("Too many arguments!")
+        
--- mary.py
+++ mary.py
@@ -4,42 +4,54 @@ import pprint
 from person import Person
 
 
-##########################################
-###   MARY WORKS, MARGAREET DOES NOT   ###
-##########################################
-
 
 
 if __name__ == '__main__':
-	mary = Person('Mary')
-	mary.myMessages = mary.messagesDict['Mary']
-	mary.otherMessages = mary.messagesDict['Margaret']
+    mary = Person('Mary')
+    mary.myMessages = mary.messagesDict['Mary']
+    mary.otherMessages = mary.messagesDict['Margaret']
 
-	if len(sys.argv) == 2:
-		if sys.argv[1] == 'ls':
-			# Print the message log
-			mary.printMessageLog()
-		else:
-			try:
-				if sys.argv[1] == 'delete':
-					for index, msg in enumerate(mary.messagesDict['Mary']):
-						print(' -' + str(index) + ': ' + msg)
-					user_input = input(" > Which one?\n > ")
-					del mary.messagesDict['Mary'][int(user_input)]
-				elif sys.argv[1] == 'add':
-					# Get user's new message and extend dictionary with it
-					user_input = input(" > What do you want to say?\n > ")
-					mary.messagesDict['Mary'].extend([str(user_input)])
-				else:
-					print(' > "{}" command not found.'.format(sys.argv[1]))
-				# Serialize any changes that were made above to the persistant file (messages.pkl)
-				mary.serialize()
-			except:
-				pass
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'ls':
+            # Print the message log
+            mary.printMessageLog()
+        else:
+            try:
+                if sys.argv[1] == 'delete':
+                    for index, msg in enumerate(mary.messagesDict['Mary']):
+                        print(' -' + str(index) + ': ' + msg)
+                    user_input = input(" > Which one?\n > ")
+                    del mary.messagesDict['Mary'][int(user_input)]
+                elif sys.argv[1] == 'add':
+                    # Get user's new message and extend dictionary with it
+                    user_input = input(" > What do you want to say?\n > ")
+                    mary.messagesDict['Mary'].extend([str(user_input)])
+                else:
+                    print(' > "{}" command not found.'.format(sys.argv[1]))
+                # Serialize any changes that were made above to the persistant file (messages.pkl)
+                mary.serialize()
+            except:
+                pass
 
-	elif len(sys.argv) == 1:
-		mary.printMessageLog()
-	else:
-		print("Too many arguments!")
+    elif len(sys.argv) == 1:
+        user_input = input("What do you want to do?\n > ")
+        
+        if user_input == 'quit':
+            print("Quitting")
+        elif user_input == 'add':
+            new_user_input = input("What do you want to say?\n > ")
+            mary.messagesDict['Mary'].extend([str(new_user_input)])
+            mary.printMessageLog()
+        elif user_input == 'delete':
+            for index, msg in enumerate(mary.messagesDict['Mary']):
+                print(' -' + str(index) + ': ' + msg)
+            new_user_input = input(" > Which one?\n > ")
+            del mary.messagesDict['Mary'][int(new_user_input)]
+            mary.printMessageLog()
+        elif user_input == 'ls':
+            mary.printMessageLog()
+        
+    else:
+        print("Too many arguments!")
 
 
